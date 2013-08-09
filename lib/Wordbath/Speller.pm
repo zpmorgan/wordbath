5400145c766c32efb542a4d2e66fe538d55d498a
Zach Morgan <zpmorgan@gmail.com>
2013-08-09 17:46:47 -0400

    gtk3 treeviews & liststores for spelling widget.

--- lib/Wordbath/Speller.pm
+++ lib/Wordbath/Speller.pm
@@ -9,6 +9,31 @@ has widget => (
   is => 'ro',
 );
 
+has _missp_ls => (
+  is => 'ro',
+  isa => 'Gtk3::ListStore',
+  builder => '_build_missp_ls',
+  lazy => 1,
+);
+has _candidates_ls => (
+  is => 'ro',
+  isa => 'Gtk3::ListStore',
+  builder => '_build_candidates_ls',
+  lazy => 1,
+);
+has _missp_view => (
+  is => 'ro',
+  isa => 'Gtk3::TreeView',
+  builder => '_build_missp_view',
+  lazy => 1,
+);
+has _candidates_view => (
+  is => 'ro',
+  isa => 'Gtk3::TreeView',
+  builder => '_build_candidates_view',
+  lazy => 1,
+);
+
 sub _build_widget{
   my $self = shift;
   my $vb = Gtk3::Box->new('vertical',3);
@@ -16,9 +41,36 @@ sub _build_widget{
   my $spell_label2= Gtk3::Label->new('DOIT! ' . $self->_check_word('12ad'));
   #$_->get_style_context->remove_class("background")
   #  for ($vb,$spell_label);
+  $vb->pack_end($self->_missp_view,0,0,0);
+  $vb->pack_end($self->_candidates_view,0,0,0);
   $vb->pack_start($spell_label,0,0,0);
   $vb->pack_end($spell_label2,0,0,0);
+  for(qw/foo bar baz/){
+    my $iter = $self->_missp_ls->append;
+    $self->_missp_ls->set($iter, 0, $_);
+  }
   return $vb;
+}
+
+sub _build_candidates_ls{
+  my $self = shift;
+  return Gtk3::ListStore->new(qw/Glib::String/);
+}
+sub _build_missp_ls{
+  my $self = shift;
+  return Gtk3::ListStore->new(qw/Glib::String/);
+}
+sub _build_candidates_view {
+  my $self = shift;
+  return Gtk3::TreeView->new($self->_candidates_ls);
+}
+sub _build_missp_view{
+  my $self = shift;
+  my $tree = Gtk3::TreeView->new($self->_missp_ls);
+  my $renderer = Gtk3::CellRendererText->new();
+  my $column = Gtk3::TreeViewColumn->new_with_attributes(werds => $renderer, text=>0);
+  $tree->append_column($column);
+  return $tree;
 }
 
 
