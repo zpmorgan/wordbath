a083723a5a8c7b0ad9f7e66bfc759e71ad9856da
Zach Morgan <zpmorgan@gmail.com>
2013-08-08 22:21:59 -0400

    very fragile integration of a spell checker.
    it probably doesn't do what you'd like it to, at the moment.

--- lib/Wordbath/Speller.pm
+++ lib/Wordbath/Speller.pm
@@ -13,12 +13,39 @@ sub _build_widget{
   my $self = shift;
   my $vb = Gtk3::Box->new('vertical',3);
   my $spell_label = Gtk3::Label->new('REMINDER: SPELL CORRECTLY');
-  my $spell_label2= Gtk3::Label->new('DOIT!');
+  my $spell_label2= Gtk3::Label->new('DOIT! ' . $self->_check_word('12ad'));
   #$_->get_style_context->remove_class("background")
   #  for ($vb,$spell_label);
   $vb->pack_start($spell_label,0,0,0);
   $vb->pack_end($spell_label2,0,0,0);
   return $vb;
+}
+
+
+use IPC::Open3;
+
+say 'Spell checker initializing. Hold your breath please.';
+my ($sp_in,$sp_out,$sp_err);
+use Symbol 'gensym'; $sp_err = gensym;
+my $pid = open3 ($sp_in, $sp_out, $sp_err, 'hunspell -');
+my $res =  <$sp_out>;
+#print $sp_in "foo\nfoo\n";
+if ($res =~ /^Hunspell/){
+  say 'Spell checker initialized. You may breathe.'
+}
+else {
+  say 'hunspell messed up?';
+}
+
+
+sub _check_word{
+  my ($self, $word) = @_;
+  if ($word =~ /\s/){
+    die "please dont pollute spell checker with whitespace. ($word)";
+  }
+  print $sp_in "$word\n";
+  my $res = <$sp_out>;
+  return $res;
 }
 
 1;
